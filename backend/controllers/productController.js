587741d5e9d92791791aeaa3a55fe574b45c10d7
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2025-01-18 22:11:31 +0530

    remove product, list product, single product api created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,6 +6,13 @@ const addProduct = async (req, res) => {
     try {
 
         const { name, description, price, category, subCategory, sizes, bestseller } = req.body
+        
+        let parsedSizes;
+        try {
+            parsedSizes = sizes ? JSON.parse(sizes) : [];
+        } catch (err) {
+            return res.json({ success: false, message: "Invalid JSON format in 'sizes'" });
+        }    
 
         const image1 = req.files.image1 && req.files.image1[0]
         const image2 = req.files.image2 && req.files.image2[0]
@@ -48,17 +55,42 @@ const addProduct = async (req, res) => {
 
 // function for list product
 const listProducts = async (req, res) => {
-    
+    try {
+        
+        const products = await productModel.find({});
+        res.json({success:true,products})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // function for removing product
 const removeProduct = async (req, res) => {
-    
+    try {
+        
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"Product Removed"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // function for single product info
 const singleProduct = async (req, res) => {
-    
+    try {
+        
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true,product})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 export { listProducts, addProduct, removeProduct, singleProduct }
