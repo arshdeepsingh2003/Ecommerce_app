5cf6bb58becc15b8eee8b3fcb6cac808a936a3e8
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2025-01-22 21:13:46 +0530

    add product functionality and minor other changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,13 +6,6 @@ const addProduct = async (req, res) => {
     try {
 
         const { name, description, price, category, subCategory, sizes, bestseller } = req.body
-        
-        let parsedSizes;
-        try {
-            parsedSizes = sizes ? JSON.parse(sizes) : [];
-        } catch (err) {
-            return res.json({ success: false, message: "Invalid JSON format in 'sizes'" });
-        }    
 
         const image1 = req.files.image1 && req.files.image1[0]
         const image2 = req.files.image2 && req.files.image2[0]
--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,24 @@ import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 
-//App config
-const app=express()
-const port=process.env.PORT || 4000
-connectDB()
-connectCloudinary
 
-//middlewares
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
 app.use(express.json())
 app.use(cors())
 
-//api endpoints
+// api endpoints
 app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 
+
 app.get('/',(req,res)=>{
-    res.send("API WORKING")
+    res.send("API Working")
 })
 
-app.listen(port,()=>console.log('Server started on PORT: '+port))
+app.listen(port, ()=> console.log('Server started on PORT : '+ port))
