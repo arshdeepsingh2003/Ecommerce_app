98d6f240639ff6329896e8ced950f40c750b6a92
arshdeepsingh2003 <136373369+arshdeepsingh2003@users.noreply.github.com>
2026-02-24 07:22:27 +0530

    remove app.listen for vercel

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,21 @@ import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
 
-
-// App Config
 const app = express()
-const port = process.env.PORT || 4000
+
 connectDB()
 connectCloudinary()
 
-// middlewares
 app.use(express.json())
 app.use(cors())
 
-// api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port, ()=> console.log('Server started on PORT : '+ port))
+export default app
